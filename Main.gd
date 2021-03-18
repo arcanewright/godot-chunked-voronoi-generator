6d5b3124c36695a2439a20d115e91ae71434866f
ArcaneWright <77860571+arcanewright@users.noreply.github.com>
2021-03-18 19:50:03 -0400

    First commit

---
+++ Main.gd
@@ -0,0 +1,129 @@
+extends Node;
+
+export var randomSeed:int;
+export var widthPerChunk: int = 5;
+export var heightPerChunk: int = 5;
+export var distBtwPoints: float = 30;
+export var distBtwVariation: float = .3;
+export var voronoiTolerance:float = .3;
+
+var view;
+
+func randomNumOnCoords(coords:Vector2, initialSeed:int):
+	var result = initialSeed
+	var randGen = RandomNumberGenerator.new();
+	randGen.seed = coords.x;
+	result += randGen.randi();
+	var newy = randGen.randi() + coords.y;
+	randGen.seed = newy;
+	result += randGen.randi();
+	randGen.seed = result;
+	result = randGen.randi();
+	return result;
+
+func generateChunkPoints(coords:Vector2, wRange:Vector2=Vector2(0, widthPerChunk), hRange:Vector2=Vector2(0, heightPerChunk)):
+	var localRandSeed = randomNumOnCoords(coords, randomSeed);
+	var initPoints = PoolVector2Array();
+	for w in range(wRange.x, wRange.y):
+		for h in range(hRange.x, hRange.y):
+			var randGen = RandomNumberGenerator.new();
+			var pointRandSeed = randomNumOnCoords(Vector2(w,h), localRandSeed);
+			randGen.seed = pointRandSeed;
+			var newPoint = Vector2(w*distBtwPoints + randGen.randf_range(-distBtwVariation, distBtwVariation)*distBtwPoints, h*distBtwPoints + randGen.randf_range(-distBtwVariation, distBtwVariation)*distBtwPoints);
+			initPoints.append(newPoint)
+	return initPoints;
+
+func generateChunkVoronoi(coords:Vector2):
+	var initPoints = generateChunkPoints(coords);
+	var sorroundingPoints = PoolVector2Array();
+	for i in range(-1, 2):
+		for j in range(-1, 2):
+			if (!(i == 0 && j == 0)):
+				var xmin = 0;
+				var xmax = 1;
+				var ymin = 0;
+				var ymax = 1;
+				if (i == -1):
+					xmin = 1 - voronoiTolerance;
+				if (i == +1):
+					xmax = voronoiTolerance;
+				if (j== -1):
+					ymin = 1 - voronoiTolerance;
+				if (j== 1):
+					ymax = voronoiTolerance;
+				var tempPoints = generateChunkPoints(Vector2(coords.x+i, coords.y+j), Vector2(xmin*widthPerChunk, xmax*widthPerChunk), Vector2(ymin*heightPerChunk, ymax*heightPerChunk));
+				var resultPoints = PoolVector2Array();
+				for point in tempPoints:
+					var tempPoint = point + Vector2(i * widthPerChunk * distBtwPoints, j * heightPerChunk * distBtwPoints);
+					resultPoints.append(tempPoint);
+				sorroundingPoints.append_array(resultPoints)
+	var allPoints = initPoints+sorroundingPoints;
+	var allDelauney = Geometry.triangulate_delaunay_2d(allPoints);
+	var triangleArray = [];
+	for triple in range(0, allDelauney.size()/3):
+		triangleArray.append([allDelauney[triple*3], allDelauney[triple*3+1], allDelauney[triple*3+2]]);
+	var circumcenters = PoolVector2Array();
+	for triple in triangleArray:
+		circumcenters.append(getCircumcenter(allPoints[triple[0]], allPoints[triple[1]], allPoints[triple[2]]));
+	var vCtrIdxWithVerts = [];
+	for point in range(initPoints.size()):
+		var tempVerts = PoolVector2Array();
+		for triangle in range(triangleArray.size()):
+			if (point == triangleArray[triangle][0] || point == triangleArray[triangle][1] || point == triangleArray[triangle][2]):
+				tempVerts.append(circumcenters[triangle]);
+		tempVerts = clowckwisePoints(initPoints[point], tempVerts)
+		vCtrIdxWithVerts.append([initPoints[point], tempVerts]);
+	
+	return vCtrIdxWithVerts;
+
+func clowckwisePoints(center:Vector2, sorrounding:PoolVector2Array):
+	var result = PoolVector2Array();
+	var angles = PoolRealArray();
+	var sortedIndexes = PoolIntArray();
+	for point in sorrounding:
+		angles.append(center.angle_to_point(point));
+	var remainingIdx = PoolIntArray();
+	for angle in range(angles.size()):
+		remainingIdx.append(angle);
+	for angle in range(angles.size()):
+		var currentMin = PI;
+		var currentTestIdx = 0;
+		for test in range(remainingIdx.size()):
+			if (angles[remainingIdx[test]] < currentMin):
+				currentTestIdx = test;
+				currentMin = angles[remainingIdx[test]];
+		sortedIndexes.append(remainingIdx[currentTestIdx]);
+		remainingIdx.remove(currentTestIdx);
+	for index in sortedIndexes:
+		result.append(sorrounding[index]);
+	return result;
+
+func getCircumcenter(a:Vector2, b:Vector2, c:Vector2):
+	var result = Vector2(0,0)
+	var midpointAB = Vector2((a.x+b.x)/2,(a.y+b.y)/2);
+	var slopePerpAB = -((b.x-a.x)/(b.y-a.y));
+	var midpointAC = Vector2((a.x+c.x)/2,(a.y+c.y)/2);
+	var slopePerpAC = -((c.x-a.x)/(c.y-a.y));
+	var bOfPerpAB = midpointAB.y - (midpointAB.x * slopePerpAB);
+	var bOfPerpAC = midpointAC.y - (midpointAC.x * slopePerpAC);
+	result.x = (bOfPerpAB - bOfPerpAC)/(slopePerpAC - slopePerpAB);
+	result.y = slopePerpAB*result.x + bOfPerpAB;
+	return result;
+
+func displayVornoiFromChunk(chunkLoc:Vector2):
+	view = get_child(0)
+	view.randSeed = randomNumOnCoords(chunkLoc, randomSeed);
+	var voronoi = generateChunkVoronoi(chunkLoc);
+	for each in voronoi:
+		view.displayPolygon(Vector2(chunkLoc.x*widthPerChunk*distBtwPoints,chunkLoc.y*heightPerChunk*distBtwPoints), each[1]);
+	view.displayPoints(Vector2(chunkLoc.x*widthPerChunk*distBtwPoints,chunkLoc.y*heightPerChunk*distBtwPoints), generateChunkPoints(chunkLoc))
+	pass
+
+func _ready():
+	for w in 10:
+		for h in 10:
+			if (w == 5 && h == 5):
+				null;
+			else:
+				displayVornoiFromChunk(Vector2(w, h));
+	pass;
---
+++ View.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+export var randSeed:int
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+func displayPoints(offset:Vector2, points:PoolVector2Array, color:Color = Color(1,1,1,1)):
+	for point in points:
+		var newPointPoly = Polygon2D.new();
+		newPointPoly.position = point + offset;
+		newPointPoly.polygon = PoolVector2Array([Vector2(-2,-2), Vector2(-2,2), Vector2(2,2), Vector2(2,-2)]);
+		newPointPoly.color = color;
+		add_child(newPointPoly)
+	pass
+
+func displayPolygon(offset:Vector2, polygon:PoolVector2Array):
+	var newPoly = Polygon2D.new();
+	var newPolyPoints = PoolVector2Array();
+	for point in polygon:
+		newPolyPoints.append(point + offset);
+	newPoly.polygon = newPolyPoints;
+	var randGen = RandomNumberGenerator.new()
+	randGen.seed = randSeed;
+	newPoly.color = Color(randGen.randf(), randGen.randf(), randGen.randf(), 1);
+	randSeed = randGen.randi();
+	add_child(newPoly)
+	pass
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
